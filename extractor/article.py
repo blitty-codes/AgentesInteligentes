2af484554d7d1920725e03cca84724cb7a0e0c0c
blitty-codes <blittynet@gmail.com>
2023-02-28 16:12:41 +0100

    feat(extractor): Fix errors, doc, reformat & tests
    - globals.py : Variable names are now constants.
    - extractor_test.py : Creation of unit tests for extractor function.
    - article.py : Added log with library `logging` for more professional
      looking. Added documentation. Now it can fetch title from IEEE
      magazine. Reformat on separate function `parse_article_data`. And
      added some Exceptions, as well as Handlers.
    
    Fix #2

--- extractor/article.py
+++ extractor/article.py
@@ -1,11 +1,11 @@
 import re
 import json
 import requests
+import logging
 from datetime import datetime
-from multiprocessing import Manager
-from multiprocessing import Process
+from multiprocessing import Process, Manager
 
-from globals import headers, base_url, issues_url
+from globals import HEADERS, ISSUES_URL, MAGAZINE_URL, BASE_URL
 
 N_THREADS = 10
 
@@ -17,25 +17,33 @@ class Article:
         self._date = datetime.now()
         self.debug = debug
         self.file_name = '' if save_to_file else None
+        self.magazine_name = self.get_magazine_name()
 
+    def get_magazine_name(self):
+        """
+        Extrae el título de la revista de IEEE
+        """
+        
+        with requests.get(MAGAZINE_URL, headers=HEADERS) as mgz:
+            return mgz.json()[0]["title"]
+        
     def get_issues(self):
         """
         Extrae el número de publicación y el número de la issue. Lo guarda en atributo _issues_values. Las issues son recorridas empezando desde el año actual hacia atrás.
         """
 
         if self.debug:
-            print(f"[+] GET {issues_url}")
+            logging.info(f"[+] GET {ISSUES_URL}")
 
-        with requests.get(issues_url, headers=headers) as issues:
+        with requests.get(ISSUES_URL, headers=HEADERS) as issues:
             for issue in issues.json()["issuelist"]:
                 if self.debug:
-                    print(f"[*] Decade: {issue['decade']}")
+                    logging.info(f"[*] Decade: {issue['decade']}")
 
                 for year in issue["years"]:
                     if self.debug:
-                        print(f"\t\t[*] Year: {year['year']}")
-                        print(
-                            f"\t\t\t[-] Number of issues: {len(year['issues'])}")
+                        logging.info(f"\t\t[*] Year: {year['year']}")
+                        logging.info(f"\t\t\t[*] Number of issues: {len(year['issues'])}")
 
                     # displayPublicationDate
                     if self._date.year >= int(year["year"]):
@@ -52,10 +60,10 @@ class Article:
         :raise Exception: No se encuentra la url o no puede ser alcanzada.
         """
 
-        issue_url = f"{base_url}rest/search/pub/{pub_number}/issue/{issue_number}/toc"
+        issue_url = f"{BASE_URL}rest/search/pub/{pub_number}/issue/{issue_number}/toc"
 
         if self.debug:
-            print(f"[+] POST {issue_url}")
+            logging.info(f"[+] POST {issue_url}")
 
         payload = {
             "isnumber": issue_number,
@@ -64,7 +72,7 @@ class Article:
         }
 
         with requests.Session() as s:
-            res = s.post(issue_url, headers=headers, json=payload)
+            res = s.post(issue_url, headers=HEADERS, json=payload)
 
             if res.status_code != 200:
                 raise Exception(
@@ -72,17 +80,91 @@ class Article:
                 )
 
             for article in res.json()["records"]:
-                # don't get articles like TOC or tutorials from IEEE
-                if "Quarter" not in article["articleTitle"] and "Editorial:" not in article["articleTitle"] and "Table of" not in article["articleTitle"]:
-                    if "htmlLink" in article.keys():
-                        self._article_links.append(article["htmlLink"])
-                    else:
-                        self._article_links.append(article["documentLink"])
+                if "htmlLink" in article.keys():
+                    self._article_links.append(article["htmlLink"])
+                else:
+                    self._article_links.append(article["documentLink"])
 
         if self.debug:
-            print(f"[*] INFO: Number of documents: {len(self._article_links)}")
+            logging.info(f"[*] Number of documents: {len(self._article_links)}")
 
-    def get_info_articles(self, ini, fini, articles_info: Manager, n):
+    def parse_article_data(self, doc: str, i: int) -> (str, str, str, [str]):
+        """
+        Dado un documento HTML de un artículo, extrae la información de titulo, abstract, fecha y palabras clave. En caso de que no exista alguno de los tres primeros mencionados, la función devolverá un Exception.
+
+        :param doc: Contenido del documento de un artículo.
+        :param i: Identificador del documento en el array (para mayor traicing de errores).
+        :exception: Si no existe titulo, abstract o fecha.
+        :return: Tupla de valores (title, abstract, date, keywords)
+        """
+
+        def parse_quarter(pub_date: str) -> str:
+            """
+            Date parser. Si la fecha no está formateada de la forma YYYY-MM-DD, entonces la convierte al primer día y mes del cuarto pasado.
+
+            :param pub_date: Fecha de publicación en string.
+            :return: DD M(Letra) YYYY
+            """
+            year = pub_date.split()[2]
+
+            if 'First' in pub_date:
+                return f"1 January {year}"
+            elif 'Second' in pub_date:
+                return  f"1 April {year}"
+            elif 'Third' in pub_date:
+                return  f"1 July {year}"
+            elif 'Fourth' in pub_date:
+                return  f"1 October {year}"
+            
+            return pub_date
+
+        pattern = re.compile(
+                    r"xplGlobal.document.metadata=(.*?);$", re.MULTILINE | re.DOTALL)
+        metadata = ''
+        # search for variable and get json in string format
+        if r := re.search(pattern, doc.content.decode('utf-8')):
+            metadata = r.group(1)
+        else:
+            raise Exception(f"No group with pattern : {re}")
+
+        # if any non-printable character in string, delete
+        metadata = re.sub(
+            r'/\\x[0-9a-zA-z][0-9a-zA-z]/g', '', metadata)
+
+        # Convert string to json
+        metadata = json.loads(metadata)
+
+        title = ''
+        abstract = ''
+        date = ''
+        keywords = []
+        
+        # TODO: Only get keywords as string, not json or whatever it fetches
+        if 'title' in metadata:
+            title = metadata['title']
+        elif 'displayDocTitle' in metadata:
+            title = metadata['displayDocTitle']
+        else:
+            raise Exception(f'This IEEE website is a pice of s*. This article does not have a Title - position: {i}')
+
+        if "keywords" in metadata:
+            keywords = metadata["keywords"]
+
+        if 'abstract' not in metadata:
+            raise Exception(f'No abstract on article: "{title}" - position: {i}')
+        else:
+            abstract = metadata['abstract']
+
+        if 'displayPublicationDate' in metadata:
+            date = parse_quarter(metadata['displayPublicationDate'])
+        elif 'dateOfInsertion' in metadata:
+            date = parse_quarter(metadata['dateOfInsertion'])
+        else:
+            raise Exception(f'No date of publication on article: "{title}" - position: {i}')
+
+        return title, abstract, date, keywords
+
+    def get_info_articles(self, ini, fini, articles_info: Manager):
         """
         Extrae "titulo del artículo", "abstract", "fecha de publicación" y "keywords" de los enlaces pasados, desde 'ini' hasta 'fini', guardándolo en el Manager `articles_info` pasado por parámetro.
 
@@ -99,62 +181,40 @@ class Article:
 
         # Get information from slice [ini, fini]
         for i in range(ini, fini):
-            article_url = f"{base_url}{self._article_links[i][1:]}toc"
+            article_url = f"{BASE_URL}{self._article_links[i][1:]}"
             if self.debug:
-                print(f"[+] GET {article_url} - article position: {i}")
+                logging.info(f"[+] GET {article_url} - article position: {i}")
 
-            with requests.get(article_url, headers=headers) as doc:
+            with requests.get(article_url, headers=HEADERS) as doc:
                 if doc.status_code != 200:
                     raise Exception(
                         f"Status code ({doc.status_code}). No article with url: {article_url}"
                     )
 
-                pattern = re.compile(
-                    r"xplGlobal.document.metadata=(.*?);$", re.MULTILINE | re.DOTALL)
-                metadata = ''
-                # search for variable and get json in string format
-                if r := re.search(pattern, doc.content.decode('utf-8')):
-                    metadata = r.group(1)
+                try:
+                    title, abstract, date, keywords = self.parse_article_data(doc, i)
+                except Exception as e:
+                    if self.debug:
+                        logging.error(f'[!] EXCEPTION: {str(e)}')
                 else:
-                    raise Exception(f"No group with pattern : {re}")
+                    if datetime.strptime(date, '%d %B %Y') <= self._date:
+                        article_data = (self.magazine_name, title, abstract, date, keywords)
+                        articles_info.append(article_data)
+                    else:
+                        logging.error(f'[!] GET article position: {i} is from dates ({date})')
 
-                # if any non-printable character in string, delete
-                metadata = re.sub(
-                    r'/\\x[0-9a-zA-z][0-9a-zA-z]/g', '', metadata)
+                    # Más llamadas, pero se asegura que no se quede abierto el fichero
+                    # en caso de problemas y se guarden X datos o todos
+                    if self.file_name is not None and article_data:
+                        file = open(self.file_name, 'a')
+                        file.write(f'{str(article_data)}\n')
+                        file.close()
 
-                # Convert string to json
-                metadata = json.loads(metadata)
-
-                # checking for missing fields
-                if 'displayDocTitle' not in metadata:
-                    metadata['displayDocTitle'] = "None"
-
-                if 'abstract' not in metadata:
-                    metadata['abstract'] = "None"
-
-                if 'displayDocTitle' not in metadata:
-                    metadata['displayDocTitle'] = "None"
-
-                if 'keywords' not in metadata:
-                    metadata['keywords'] = "None"
-
-                if datetime.strptime(metadata['displayPublicationDate'], '%d %B %Y') <= self._date:
-                    article_data = (metadata['displayDocTitle'], metadata['abstract'],
-                                    metadata['displayPublicationDate'], metadata['keywords'])
-                    articles_info.append(article_data)
-
-                # Más llamadas, pero se asegura que no se quede abierto el fichero
-                # en caso de problemas y se guarden X datos o todos
-                if self.file_name is not None and article_data:
-                    file = open(self.file_name, 'a')
-                    file.write(f'{str(article_data)}\n')
-                    file.close()
-
-    def get_all_articles(self, n) -> [(str, str, str, str)]:
+    def get_all_articles(self) -> [(str, str, str, str, str)]:
         """
         Helper function. Ayuda a coger la información de varios artículos
 
-        :return: Una lista compuesta por [(str, str, str, str)]
+        :return: Una lista compuesta por [(str, str, str, str, str)]
         """
 
         # lista de acceso común para todos los procesos involucrados
@@ -162,9 +222,8 @@ class Article:
         articles_info = manager.list()
 
         # gestión de slices para cada proceso (tamaño y función a ejecutar)
-        n = len(self._article_links)
-        size = n/N_THREADS
-
+        n_art = len(self._article_links)
+        size = n_art/N_THREADS
         # creación de procesos
         processes = []
 
@@ -173,7 +232,7 @@ class Article:
             fini = ini + size
 
             proc = Process(target=self.get_info_articles,
-                           args=(ini, fini, articles_info, n))
+                           args=(ini, fini, articles_info))
             processes.append(proc)
 
         for p in processes:
@@ -186,7 +245,7 @@ class Article:
 
         return articles_info
 
-    def extract(self, n, since=None):
+    def extract(self, n, since=None) -> [(str, str, str, str, [str])]:
         """Extrae la información de ilos últimos n artículos hasta since
 
         :param n: El número de artículos de los que extraer datos. Debe
@@ -198,9 +257,12 @@ class Article:
             siguiente forma: (str, str, str, str, List[str])
         """
 
+        if self.debug:
+            logging.basicConfig(filename=f'app-n_{n}_since_{since}.log', filemode='w', format='%(levelname)s - %(message)s', encoding='utf-8', level=logging.INFO)
+
         if since is not None:
             self._date = since
-            self._date.replace(year = self._date.year + 1)
+            self._date = self._date.replace(year=self._date.year + 1)
 
         self.get_issues()
 
@@ -210,7 +272,7 @@ class Article:
 
             f = open(self.file_name, "w")
             f.close()
-            print(f"[*] INFO: File named: {self.file_name} created")
+            logging.info(f"[*] File named: {self.file_name} created")
 
         for issue in self._issues_values:
             # get articles from one issue
@@ -220,7 +282,7 @@ class Article:
             if n <= len(self._article_links):
                 break
 
-        articles = self.get_all_articles(n)
+        articles = self.get_all_articles()
+        articles = sorted(articles, key=lambda x: datetime.strptime(x[3], '%d %B %Y'))
 
-        return sorted(articles, key=lambda x: datetime.strptime(x[2], '%d %B %Y'))[:n]
-
+        return articles[:n]
---
+++ extractor/extractor_test.py
@@ -0,0 +1,45 @@
+import unittest
+from datetime import datetime
+
+from article import Article
+
+
+class TestExtractor(unittest.TestCase):
+    def test_n_10_since_2000(self):
+        """
+        Test can fetch information from 10 articles since 2000
+        """
+        # print("\n[+] Starting test_n_10_since_2000")
+        art = Article(debug=True, save_to_file=False)
+        articles = art.extract(
+                n=10,
+                since=datetime.strptime('2000-1-23', '%Y-%m-%d'))
+        
+        self.assertEqual(len(articles), 10)
+    
+    def test_n_50_since_2000(self):
+        """
+        Test can fetch information from 50 articles since 2000
+        """
+        # print("\n[+] Starting test_n_50_since_2000")
+        art = Article(debug=True, save_to_file=False)
+        articles = art.extract(
+                n=50,
+                since=datetime.strptime('2000-1-23', '%Y-%m-%d'))
+        
+        self.assertEqual(len(articles), 17)
+    
+    
+    def test_n_100(self):
+        """
+        Test can fetch information from 50 articles since "today"
+        """
+        # print("\n[+] Starting test_n_100")
+
+        art = Article(debug=False, save_to_file=False)
+        articles = art.extract(n=50)
+
+        self.assertEqual(len(articles), 50)
+
+if __name__ == '__main__':
+    unittest.main() 
