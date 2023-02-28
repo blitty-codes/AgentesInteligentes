0109fb362b2782248c4954d9dcdaea41a6fafd5f
Alvaro Tena Tamayo <alvarotenatamayo@gmail.com>
2023-02-28 01:55:12 +0100

    Args parser

--- extractor/neuralhive.py
+++ extractor/neuralhive.py
@@ -20,12 +20,21 @@ enlaces:
     - https://realpython.com/python-first-steps/
 """
 
+import argparse
 from article import Article
 from datetime import datetime
 
 
 if __name__ == '__main__':
-    art = Article(debug=True, save_to_file=False)
-    articles = art.extract(50, since=datetime.fromisoformat('2019-12-01'))
+    parser = argparse.ArgumentParser(description='Agente extractor de datos de artículos para la página: https://ieeexplore.ieee.org/xpl/RecentIssue.jsp?punumber=9739')
+    parser.add_argument('-n', '--num_files', type=int, required=True, help='Numero de articulos a extraer')
+    parser.add_argument('--since', type=lambda s: datetime.strptime(s, '%Y-%m-%d'), help='Fecha inicial para la extracción con formato YYYY-MM-DD')
+    parser.add_argument('-d', '--debug', action='store_true', help='Modo debug')
+    parser.add_argument('--save', action='store_true', help='Guardar extracción en fichero')
+
+    args = parser.parse_args()
+
+    art = Article(debug=args.debug, save_to_file=args.save)
+    articles = art.extract(n=args.num_files, since=args.since)
 
     print(len(articles))
